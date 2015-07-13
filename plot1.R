a428a5eb11a9a14da82f1f37fd6156bfb4d4a262
Pueblo2 <rgenova@securewindows.biz>
2015-07-12 21:50:45 -0400

    Coursera July 2015 Exploring Data
    Data Scientist Signature Track Coursework

---
+++ plot1.R
@@ -0,0 +1,15 @@
+##  RDS Data Sample
+
+NEI <- readRDS("Data/summarySCC_PM25.rds")
+SCC <- readRDS("Data/Source_Classification_Code.rds")
+
+## Samples data for testing
+NEIsample <- NEI[sample(nrow(NEI), size = 1000, replace = F), ]
+
+## Aggregated
+Emissions <- aggregate(NEI[, 'Emissions'], by = list(NEI$year), FUN = sum)
+Emissions$PM <- round(Emissions[, 2] / 1000, 2)
+
+png(filename = "plot1.png")
+barplot(Emissions$PM, names.arg = Emissions$Group.1, main = expression('Total Emission of PM'[2.5]), xlab = 'Year', ylab = expression(paste('PM', ''[2.5], ' in Kilotons')))
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,14 @@
+##  RDS Data 
+
+NEI <- readRDS("Data/summarySCC_PM25.rds")
+SCC <- readRDS("Data/Source_Classification_Code.rds")
+
+# Samples  for Testing
+NEIsample <- NEI[sample(nrow(NEI), size = 5000, replace = F), ]
+
+# Subset data and appends two years to data frame
+MD <- subset(NEI, fips == '24510')
+
+png(filename = 'plot2.png')
+barplot(tapply(X = MD$Emissions, INDEX = MD$year, FUN = sum), main = 'Total Emission in Baltimore City, MD', xlab = 'Year', ylab = expression('PM'[2.5]))
+dev.off()
